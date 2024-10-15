ef31e9528b927e145e2188aa24072d89f04ced4c
sibika2003 <sibikacse@gmail.com>
2024-10-15 03:12:12 -0700

    first commit

---
+++ reactjs/src/App.js
@@ -0,0 +1,51 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return 
+      <head>
+     <body>
+        <center>
+        <h1>sibika R</h1>
+        <hr>
+        <h4>Mobile No:8148724140
+        Email id:sibikacse@gmail.com
+        Address:sirkazhi 609115
+        </h4>
+</hr>
+<p>
+  <h4>Summary</h4>
+  </p>
+    <h4>I am innovative college student.Through Hand on project,internship
+        ls and knowledge to make
+         a meaningful impact in the world</h4>
+         <hr>
+<P>
+  <h3>Skills</h3>
+  </P> 
+<h4>
+     Github <div>
+     Git</div>
+     HTML
+     <div>
+     Team work and Collaboration
+     </div>
+     </h4>
+    </hr>
+<p>
+  <h3>Education</h3>
+  </p>
+</center>
+<h5>
+  <div>Bachelor of Computer science and Engineering,
+    At vetri vinayaha college of engineering and technoogy, thottiyam.</div>
+    year:2024</h5>
+    
+   <p>
+    <h3>Languages</h3>
+    </p>
+    <h5>English First language</h5>
+    
+    </body>
+    </head>
+}
